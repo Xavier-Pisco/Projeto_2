952648936f79e9f5a48fee1e4646da119ff2d239
Xavier-Pisco <x.pisco@gmail.com>
2019-05-02 19:51:17 +0100

    Main back

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "Address.h"
-//#include "Agency.h"
-//#include "Menus.h"
+
+#include "Agency.h"
+#include "Menus.h"
 // #include "utils.h"
 
 
 int main(){
+  Agency agency(AGENCY_FILE_NAME);   // create the agency
 
-	Address a("Rua de cima", 123, "2", "123", "Douro vinhateiro - Porto, Régua, Pinhão, Vila Real");
-
-  //Agency agency(AGENCY_FILE_NAME);   // create the agency
-
-  //mainMenu(agency); // initial menu inicial with the major options of the application
+  mainMenu(agency); // initial menu inicial with the major options of the application
   
 	return 0;
 }
